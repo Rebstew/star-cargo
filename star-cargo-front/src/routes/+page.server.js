7963628fcbc848feaf9416ed388b75c88955ff69
rebstew <w3b5t3r56@gmail.com>
2025-01-08 16:00:41 +0100

    removes angular for svelte for the front - fuck angular all my homies hate angular

---
+++ star-cargo-front/src/routes/+page.server.js
@@ -0,0 +1,23 @@
+
+
+export function load() {
+    return {
+        popularCharts: [
+            {
+                name: 'Chart 1 for the ship Executor',
+                description: 'A beautiful chart for the Executor',
+                id: '8aecf1b0-0b1b-11ec-9a03-0242ac130003',
+            },
+            {
+                name: 'Avenger v2',
+                description: 'Better avenger chart',
+                id: 'd6f5e5b0-0b1b-11ec-9a03-0242ac130003',
+            },
+            {
+                name: 'Avenger v3',
+                description: 'An even better avenger chart',
+                id: '3d1f4b50-0b1c-11ec-9a03-0242ac130003',
+            }
+        ]
+    };
+}
---
+++ star-cargo-front/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	let { data } = $props();
+</script>
+
+<div class="main-content">
+    <h2>Star Cargo</h2>
+
+    <p>Besoin d'optimiser votre cargo bla bla bla. StarcarGo grid est fait pour vous ! 
+        Créez votre Schéma sur le vaisseau de vos rêves bla bla bla.</p>
+    <a href="chartEdit" class="cta-button">Create your first chart</a>
+</div>
+
+<section class="chart-section">
+    <h3>Most popular charts</h3>
+    <div class="chart-grid">
+        {#each data.popularCharts as popularChart}
+        <div class="chart-card">
+            <img src="assets/images/ship.jpg" alt="Ship image">
+            <h4>{popularChart.name}</h4>
+            <p>{popularChart.description}</p>
+            <a href="chart/{popularChart.id}" class="cta-button">Voir le schéma</a>
+        </div>
+        {/each}
+    </div>
+</section>
